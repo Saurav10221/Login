64e79eabad82c9d05b018cf6f31e39c13c9b1478
Saurav10221 <122508636+Saurav10221@users.noreply.github.com>
2023-01-18 14:31:33 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,3 +1,30 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+ 
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyD87KFZBPE9T6NjATNAY1e4jdSznRQRXSk",
+    authDomain: "login-2ae9e.firebaseapp.com",
+    projectId: "login-2ae9e",
+    storageBucket: "login-2ae9e.appspot.com",
+    messagingSenderId: "1030010808330",
+    appId: "1:1030010808330:web:fb25fee89d3851526a97f3",
+    measurementId: "G-R6YKZKDWDG"
+  };
+
+ 
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
 var provider = new firebase.auth.GithubAuthProvider();
 
 function githubSignin() {
